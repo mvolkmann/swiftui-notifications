26612fe1400bcbc47775e863eec8fd16bfa5c93f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-05-09 09:27:44 -0500

    Initial Commit

---
+++ swiftui-notifications/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-notifications
+//
+//  Created by Mark Volkmann on 5/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-notifications/swiftui_notificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_notificationsApp.swift
+//  swiftui-notifications
+//
+//  Created by Mark Volkmann on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_notificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
